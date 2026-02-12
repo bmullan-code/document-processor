4da2770c6ce103a34637b01af6e575b888d64f40
Barry Mullan <bmullan@google.com>
2026-02-12 08:08:53 -0500

    updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -98,6 +98,7 @@ resource "google_cloud_run_v2_service" "api_service" {
   name     = "doc-processor-api"
   location = var.region
   ingress  = "INGRESS_TRAFFIC_ALL"
+  deletion_protection = false
 
   template {
     service_account = google_service_account.service_account.email
